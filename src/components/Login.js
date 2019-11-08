db2b08a78074920746a241eb63cbc6f9c4feb3c6
clukhei <clukhei@gmail.com>
2019-11-08 23:46:01 +0800

    Login form without validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,89 @@
 import React from "react";
 import Loginimage from "C:/Users/Lu Theng/Desktop/Techladies bootcamp/Reach project/reach-frontend/src/images/Group 5.svg";
 import Logo from "C:/Users/Lu Theng/Desktop/Techladies bootcamp/Reach project/reach-frontend/src/images/Logo.svg";
+import { Link } from 'react-router-dom';
 
 
-const Login = () =>
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+      formErrors: {
+        email: "",
+        password: ""
+      }
+    };
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log("Submitting");
+    console.log(this.state);
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  render() {
+    const { email, password } = this.state;
+    return (
+      <div>
+        <img src={Loginimage} className="login-img" alt="Login" />
+        <ul className="login-form">
+          <li className=  "login-form-list">
+            <img src={Logo} className="logo" alt="Logo" />
+          </li>
+          <form onSubmit={this.handleSubmit} >
+            <li>
+              <label htmlFor="email">Email Address</label>
+            </li>
+            <li className=  "login-form-list">
+              <input
+                type="text"
+                name="email"
+                placeholder=" Enter your email address"
+                value={email}
+                onChange={this.handleChange}
+                className="textbox"
+              />
+            </li>
+            <li > 
+              <label htmlFor="password">Password</label>
+            </li>
+            <li className=  "login-form-list">
+              <input
+                type="text"
+                name="password"
+                placeholder=" Enter your password"
+                value={password}
+                onChange={this.handleChange}
+                className="textbox"
+              />
+            </li>
+            <li className=  "login-form-list">
+              <Link to="#">Forget password?</Link>
+            </li>
+            <button className="login-button">
+              <span className="login-button-text">Login</span>
+            </button>
+          </form>
+
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Login;
+
+/*
+const Login = () => (
   <div>
     <img src={Loginimage} className="login-img" alt="Login" />
     <ul className="login-form">
@@ -23,9 +103,5 @@ const Login = () =>
 
     </ul>
   </div>
-
-
-
-export default Login;
-
-
+);
+ */
