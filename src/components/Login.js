84a3dbcf4593f55f5a317dec29e15348c79cfe25
clukhei <clukhei@gmail.com>
2019-11-09 16:57:09 +0800

    refactor Login to functional component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Loginimage from "../images/Group 5.svg";
 import Logo from "../images/Logo.svg";
 import { Link } from 'react-router-dom';
 
-const formValid =  formErrors => {
+
+const initialState = {
+  name: "",
+  email: ""
+};
+
+
+const formValid = formErrors => {
   let valid = true;
 
   // validate form errors being empty
@@ -12,118 +19,78 @@ const formValid =  formErrors => {
   });
 
   return valid;
-}
+};
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      email: "",
-      password: "",
-      formErrors: {
-        email: "",
-        password: ""
-      }
-    };
-  }
-
-  handleChange = e => {
-    this.setState({
+function Login() {
+  const [state, setState] = useState(initialState)
+  const handleChange = e => {
+    setState({
       [e.target.name]: e.target.value
     });
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
-    if (formValid(this.state)) {
+    if (formValid(state)) {
       console.log(`
         --SUBMITTING--
-        Email: ${this.state.email}
-        Password: ${this.state.password}
+        Email: ${state.email}
+        Password: ${state.password}
       `);
-    } else {this.setState({
-      error: "Your email and password do not match. Please try again"
-  });
+    } else {
+      setState({
+        error: "Your email and password do not match. Please try again"
+      });
     }
   }
-
-  render() {
-    console.log(this.state);
-    const { email, password } = this.state;
-    return (
-      <div>
-        <img src={Loginimage} className="login-img" alt="Login" />
-        <ul className="login-form">
-          <li className=  "login-form-list">
-            <img src={Logo} className="logo" alt="Logo" />
+  return (
+    <div>
+      <img src={Loginimage} className="login-img" alt="Login" />
+      <ul className="login-form">
+        <li className="login-form-list">
+          <img src={Logo} className="logo" alt="Logo" />
+        </li>
+        <form onSubmit={handleSubmit} >
+          <li>
+            <label htmlFor="email">Email Address</label>
           </li>
-          <form onSubmit={this.handleSubmit} >
-            <li>
-              <label htmlFor="email">Email Address</label>
-            </li>
-            <li className=  "login-form-list">
-              <input
-                type="text"
-                name="email"
-                placeholder=" Enter your email address"
-                value={email}
-                onChange={this.handleChange}
-                className="textbox"
-              />
-            </li>
-            <li > 
-              <label htmlFor="password">Password</label>
-            </li>
-            <li className=  "login-form-list">
-              <input
-                type="text"
-                name="password"
-                placeholder=" Enter your password"
-                value={password}
-                onChange={this.handleChange}
-                className="textbox"
-              />
-            </li>
-            <li className=  "login-form-list">
-              <Link to="#">Forget password?</Link>
-            </li>
-            {this.state.error && <p className= "error-msg">{this.state.error}</p>}
-            <button className="login-button">
-              <span className="login-button-text">Login</span>
-            </button>
-          </form>
-         
+          <li className="login-form-list">
+            <input
+              type="text"
+              name="email"
+              placeholder=" Enter your email address"
+              value={state.email}
+              onChange={handleChange}
+              className="textbox"
+            />
+          </li>
+          <li>
+            <label htmlFor="password">Password</label>
+          </li>
+          <li className="login-form-list">
+            <input
+              type="text"
+              name="password"
+              placeholder=" Enter your password"
+              value={state.password}
+              onChange={handleChange}
+              className="textbox"
+            />
+          </li>
+          <li className="login-form-list">
+            <Link to="#">Forget password?</Link>
+          </li>
+          {state.error && <p className="error-msg">{state.error}</p>}
+          <button className="login-button">
+            <span className="login-button-text">Login</span>
+          </button>
+        </form>
 
-        </ul>
-      </div>
-    );
-  }
+
+      </ul>
+    </div>
+  );
 }
 
 export default Login;
-
-/*
-const Login = () => (
-  <div>
-    <img src={Loginimage} className="login-img" alt="Login" />
-    <ul className="login-form">
-      <form onSubmit>
-        <li className=  "login-form-list">
-          <img src={Logo} className="logo" alt="Logo" />
-        </li>
-        <li className=  "login-form-list"> Email Address<br className= "login-br"/>
-          <input type="text" name="email" placeholder=" Enter your email address" className="textbox" />
-        </li>
-        <li className=  "login-form-list">Password <br className= "login-br"/>
-          <input type="text" name="password" placeholder=" Enter your password" className="textbox" />
-        </li>
-        <li className=  "login-form-list">Forgot password? </li>
-        <li className=  "login-form-list"><button className = "login-button"><span className= "login-button-text">Login</span></button> </li>
-      </form>
-
-    </ul>
-  </div>
-);
- */
