929622c9bff0e5ad8ae2458decf9cdc79909e3ed
Ler Ka Leng <kalengzonline@hotmail.com>
2019-11-19 23:35:37 +0800

    remove duplicate login components on App.js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import Navbar from './components/navbar';
 import './App.css';
-import Login from './components/Login';
-import { BrowserRouter, Route, Link , Switch} from 'react-router-dom';
+
 
 function App() {
   return (
 
     <div className="App">
       <Navbar />
-      <BrowserRouter>
-        <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+
     </div>
 
   );
