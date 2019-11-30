2ce7e437bbd2903e998a45a3e5d50aeccd35b51b
Ler Ka Leng <kalengzonline@hotmail.com>
2019-11-30 18:29:19 +0800

    dashboard api changes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { VictoryLine, VictoryChart, VictoryTheme } from 'victory';
 import Box from './Box'
 import DatePicker from "react-datepicker";
@@ -8,15 +8,82 @@ import DonationAmount from "./DonationAmount"
 import KeyStatistics from "./KeyStatistics" 
 import ByIntent from "./ByIntent"
 
-const fakeData = [
-    { x: "cats", y: 55, fill: "#0a2" },
-    { x: "dogs", y: 124, fill: "blue" },
-    { x: "rabbits", y: 20, fill: "orange" }
-  ];
+// const fakeData = [
+//     { x: "cats", y: 55, fill: "#0a2" },
+//     { x: "dogs", y: 124, fill: "blue" },
+//     { x: "rabbits", y: 20, fill: "orange" }
+//   ];
+
   
   const Dashboard  = () => { 
-    const [startDate, setStartDate] = useState(new Date("2019-11-20"));
-    const [endDate, setEndDate] = useState(new Date());
+     
+    const today = new Date()
+    // use state start and end
+    const [startDate, setStartDate] = useState(today.setMonth(today.getMonth()-3))
+    const [endDate, setEndDate] = useState(today)
+    const [donationAmt, setDonationAmt] = useState([]);
+    const [totalDonationAmt, setTotalDonationAmt] = useState(0);
+    const [totalNoOfDonations, setTotalNoOfDonations] = useState(0); 
+    const [noOfDonationBySource, setNoOfDonationBySource] = useState([]);  
+    const [donationAmtByIntent, setDonationAmtByIntent] = useState([]);
+
+    const fetchData = async (start, end) => {
+      const response = await fetch (
+        '/dashboard?startDate='+start+'&endDate='+end
+      ).then(resp => resp.json())
+        .then(data => {
+          data = {
+            "startDate": "2019-09-23",
+            "endDate": "2019-11-23",
+            "donationAmt": [
+                {"date": "2019-09-23", "amount": "213"},
+                {"date": "2019-09-24", "amount": "345"},
+                {"date": "2019-09-25", "amount": "2343"},
+                {"date": "2019-09-26", "amount": "354"},
+                {"date": "2019-09-27", "amount": "76"},
+                {"date": "2019-09-28", "amount": "6765"},
+                {"date": "2019-09-29", "amount": "445"},
+                {"date": "2019-09-30", "amount": "97"},
+                {"date": "2019-10-01", "amount": "4567"},
+                {"date": "2019-10-02", "amount": "458"},
+                {"date": "2019-10-03", "amount": "89"},
+                {"date": "2019-10-04", "amount": "889"}
+            ],
+            "totalDonationAmt": "12154.00",
+            "totalNoOfDonations": "6328",
+            "noOfDonationBySource": [
+                { "sourceName": "bi-monthly charity dinner", "noOfDonations": 3238 },
+                { "sourceName": "church", "noOfDonations": 832 },
+                { "sourceName": "online (Beverity)", "noOfDonations": 290 },
+                { "sourceName": "online (REACH)", "noOfDonations": 102 },
+                { "sourceName": "others", "noOfDonations": 88 },
+            ],
+            "donationAmtByIntent": [
+                { "amount": 4900, "intent": "counselling"},
+                { "amount": 5500, "intent": "family"},
+                { "amount": 3800, "intent": "senior"},
+                { "amount": 4200, "intent": "youth"},
+                
+              ]
+          }
+          setDonationAmt(data.donationAmt)
+          setTotalDonationAmt(data.totalDonationAmt)
+          setTotalNoOfDonations(data.totalNoOfDonations)
+          setNoOfDonationBySource(data.noOfDonationBySource)
+          setDonationAmtByIntent(data.donationAmtByIntent)
+        })
+        .catch(err => {console.log("err: ", JSON.stringify(err))});
+    }
+
+    fetchData(startDate, endDate);
+
+    useEffect( () => { fetchData(startDate, endDate).then(res => res); }
+
+      // setStartDate(response.startDate) // startDate = response.startDate
+      // setEndDate(response.endDate)
+      
+    , [startDate, endDate]);
+
     return (
       <>
         <div className = "dashboard-header wrap">
