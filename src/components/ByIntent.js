176658fa1e669a7b51cd7a48bce9ce895ae1f290
Ler Ka Leng <kalengzonline@hotmail.com>
2019-11-30 15:11:28 +0800

    wip

---
+++ src/components/ByIntent.js
@@ -0,0 +1,93 @@
+import React from "react";
+import TwoHands from "../images/twohandsdonation.svg";
+import Crown from "../images/crown.svg";
+import Dashboard from "./Dashboard"
+import { VictoryPie, VictoryTooltip, VictoryAxis, VictoryContainer } from "victory";
+
+const fakeData = [
+  { x: "Bi-monthly Charity Dinner", y: 2320, fill: "#FFA001" },
+  { x: "Online (Benevity)", y: 823, fill: "#FE5366" },
+  { x: "Online (REACH)", y: 390, fill: "#FF9A85" },
+  { x: "Church Donations", y: 402, fill: "#80485B" },
+  { x: "Others", y: 288, fill: "#CB87B0" }
+];
+
+/* highest value of y in fakeDATA */
+/* const highestDonorIntent = Math.max.apply(Math, fakeData.map(function(o){return o.y;})) */
+
+const ByIntent = () => (
+  <div className="chart">
+
+    <VictoryPie
+      innerRadius={103}
+      containerComponent={<VictoryContainer responsive={false} />}
+      height={350}
+      width={350}
+      /* labelComponent={<VictoryTooltip activateData={true} flyoutStyle= {{display: "none"}} />} */
+      labelComponent={<VictoryAxis tickFormat={() => ''} />}
+      padAngle={8}
+      data={fakeData}
+      style={{
+        data: {
+          fill: ({ datum }) => datum.fill
+        }
+      }}
+    >
+    </VictoryPie>
+
+    <div>
+        <img src={TwoHands} className="twohands" />
+    </div>
+
+    <div className="legend-container">
+      <div className="byintent-row1 byintentborder">
+        <div className="flex"><img src={Crown} /><h1 className="byintent-crownvalue">{fakeData[0].y}</h1></div>
+        <div className="flex">
+          <div className="legend brightorange"></div>
+          <div className="byintent-name"> {fakeData[0].x}</div>
+        </div>
+      </div>
+
+      <div className="flex">
+        <div className="byintent-row byintentborder">
+          <h1 className="byintent-value">{fakeData[1].y}</h1>
+          <div className="flex">
+            <div className="legend pink"></div>
+            <div className="byintent-name">{fakeData[1].x}</div>
+          </div>
+        </div>
+
+        <div className="byintent-row byintentborder">
+          <h1 className="byintent-value">{fakeData[2].y}</h1>
+          <div className="flex">
+            <div className="legend lightpink"></div>
+            <div className="byintent-name">{fakeData[2].x}</div>
+          </div>
+        </div>
+      </div>
+      <div className="flex">
+        <div className="byintent-row">
+          <h1 className="byintent-value">{fakeData[3].y}</h1>
+          <div className="flex">
+            <div className="legend darkpurple"></div>
+            <div className="byintent-name">{fakeData[3].x}</div>
+          </div>
+        </div>
+
+        <div className="byintent-row">
+          <h1 className="byintent-value">{fakeData[4].y}</h1>
+          <div className="flex">
+            <div className="legend lilac"></div>
+            <div className="byintent-name">{fakeData[4].x}</div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
+
+);
+
+export default ByIntent;
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
-import { VictoryPie, VictoryLine, VictoryChart, VictoryTheme } from 'victory';
+import { VictoryLine, VictoryChart, VictoryTheme } from 'victory';
 import Box from './Box'
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import ByIntent from './ByIntent'
+import donationsCircle from "../images/donations-circle.svg";
+import twoHandsDonation from "../images/two-hands-donation.svg";
+import Steps from "../images/steps.png";
 
 const fakeData = [
     { x: "cats", y: 55, fill: "#0a2" },
@@ -11,82 +15,90 @@ const fakeData = [
   ];
   
   const Dashboard  = () => { 
-    const [startDate, setStartDate] = useState(new Date());
+    const [startDate, setStartDate] = useState(new Date("2019-11-20"));
     const [endDate, setEndDate] = useState(new Date());
     return (
       <>
-      <div className = "dashboard-header wrap">
-        <h1>Dashboard</h1>
-          <div className ="d-flex">
-            <div>
-              <label htmlFor="startDate"> From &nbsp; {"      "}</label>
-
-              <DatePicker id="startDate" selected={startDate} onChange={date => setStartDate(date)} selectsStart 
-              startDate={startDate}
-        endDate={endDate}/>
-            </div>
+        <div className = "dashboard-header wrap">
+          <h1>Dashboard</h1>
+            <div className ="d-flex">
               <div>
-                <label htmlFor="endDate"> To &nbsp; {"      "}</label>
-                <DatePicker id="endDate" selected={endDate} onChange={date => setEndDate(date)}  selectsEnd
-        endDate={endDate}
-        minDate={startDate} />
+                <label htmlFor="startDate"> From &nbsp; {"      "}</label>
+
+                <DatePicker id="startDate" selected={startDate} onChange={date => setStartDate(date)} selectsStart 
+                startDate={startDate} endDate={endDate}/>
               </div>
+                <div>
+                  <label htmlFor="endDate"> To &nbsp; {"      "}</label>
+                  <DatePicker id="endDate" selected={endDate} onChange={date => setEndDate(date)}  selectsEnd endDate={endDate} minDate={startDate} />
+                </div>
+                <button className="button orangebutton ml-auto">View Donor List</button>
+            </div>
+        </div>
+
+        <div className = "wrap">     
+        <h1>Donation Amount</h1>
+          <Box>
+          <div className="line-chart">
+          
+              <VictoryChart
+              theme={VictoryTheme.material}
+              height={200}
+              width={300}
+              >
+              <VictoryLine
+                  style={{
+                  data: { stroke: "#FE5366" },
+                  parent: { border: "1px solid #CC7503"},
+                  }}
+                  data={[
+                  { x: 1, y: 2 },
+                  { x: 2, y: 3 },
+                  { x: 3, y: 5 },
+                  { x: 4, y: 4 },
+                  { x: 5, y: 7 }
+                  ]}
+              />
+              </VictoryChart>
           </div>
-      </div>
-      <div className = "wrap">
+          </Box>
+
+          <div className = "keystats">
+          <h1>Key Statistics</h1>
+            <Box>
+            <div>
+               <img src={donationsCircle} className="donationscircle" />
+               <div className = "totaldonationamt">$12,154</div>
+                 <div className = "keystatslabel">Total Donation Amount</div>
+            </div>
+
+            </Box>
+
+            <Box>
+            <div>
+               <img src={twoHandsDonation} className="twoHandsDonation" /></div>
+               <div className = "totalnumberdonors">6329</div>
+                <div className = "keystatslabel">Total Number of Donors</div>
+            
+
+            </Box>
+            
+            <img src={Steps} className="steps" />
+
+          </div>
+
+          <h2>Total Number of Donations</h2>
+          <h1>By Intent</h1>
+            <Box>
+              <ByIntent />
+            </Box>
 
           
-
-        <Box>
-
-        <div className="line-chart">
-            <VictoryChart
-            theme={VictoryTheme.material}
-            height={200}
-            width={300}
-            >
-            <VictoryLine
-                style={{
-                data: { stroke: "#FE5366" },
-                parent: { border: "1px solid #CC7503"},
-                }}
-                data={[
-                { x: 1, y: 2 },
-                { x: 2, y: 3 },
-                { x: 3, y: 5 },
-                { x: 4, y: 4 },
-                { x: 5, y: 7 }
-                ]}
-            />
-            </VictoryChart>
-
+          
         </div>
-        </Box>
-        </div>
-        </>
+      </>
+        
       );
     }
-  // function Dashboard() {
-  //     const fakeData = [
-  //         { x: "cats", y: 55, fill: "#0a2" },
-  //         { x: "dogs", y: 124, fill: "blue" },
-  //         { x: "rabbits", y: 20, fill: "orange" }
-  //       ];
-    
-  //     return (
-  //       <div>
-  //         <VictoryChart theme={VictoryTheme.material}>
-  //             </VictoryChart>
-  //         <VictoryLine
-  //          style={{
-  //             data: { stroke: "#c43a31" },
-  //             parent: { border: "1px solid #ccc"}
-  //             }}
-  //          data={[
-  //              { x: 1, y: 2 },
-  //             { x: 2, y: 3 },
-  //       </div>
-  //     );
-  //   };
   
   export default Dashboard;
