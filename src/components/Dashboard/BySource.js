050d4b11c4f2469e6fa07d7fc9f3a7bc3b9d2fe0
chong lu khei <clukhei@gmail.com>
2019-11-30 18:34:33 +0800

    dashboard grid work in progress

--- src/components/Dashboard/BySource.js
+++ src/components/Dashboard/BySource.js
@@ -1,11 +1,8 @@
 import React from "react";
 import TwoHands from "../../images/twohandsdonation.svg";
 import Crown from "../../images/crown.svg";
-import {
-  VictoryPie,
-  VictoryAxis,
-  VictoryContainer
-} from "victory";
+import Box from "./Box";
+import { VictoryPie, VictoryAxis, VictoryContainer } from "victory";
 
 const fakeData = [
   { x: "Bi-monthly Charity Dinner", y: 2320, fill: "#FFA001" },
@@ -16,76 +13,84 @@ const fakeData = [
 ];
 
 const BySource = () => (
-  <div className="chart">
-    <div className="piechart-holder">
-      <div className="piechart-img">
-        <img src={TwoHands} className="twohands" />
-      </div>
-
-      <VictoryPie
-        innerRadius={103}
-        containerComponent={<VictoryContainer responsive={false} />}
-        height={350}
-        width={350}
-        labelComponent={<VictoryAxis tickFormat={() => ""} />}
-        padAngle={8}
-        data={fakeData}
-        style={{
-          data: {
-            fill: ({ datum }) => datum.fill
-          }
-        }}
-      ></VictoryPie>
+  <>
+    <div>
+      <header>Total Number of Donations</header>
+      <h1>By Source</h1>
     </div>
+    <Box>
+      <div className="chart">
+        <div className="piechart-holder">
+          <div className="piechart-img">
+            <img src={TwoHands} className="twohands" />
+          </div>
 
-    <div className="legend-container">
-      <div className="bysource-row1 bysourceborder">
-        <div className="flex">
-          <img src={Crown} />
-          <h1 className="bysource-crownvalue">{fakeData[0].y}</h1>
+          <VictoryPie
+            innerRadius={103}
+            containerComponent={<VictoryContainer responsive={false} />}
+            height={350}
+            width={350}
+            labelComponent={<VictoryAxis tickFormat={() => ""} />}
+            padAngle={8}
+            data={fakeData}
+            style={{
+              data: {
+                fill: ({ datum }) => datum.fill
+              }
+            }}
+          ></VictoryPie>
         </div>
-        <div className="flex">
-          <div className="legend brightorange"></div>
-          <div className="bysource-name"> {fakeData[0].x}</div>
+
+        <div className="legend-container">
+          <div className="bysource-row1 bysourceborder">
+            <div className="flex">
+              <img src={Crown} />
+              <h1 className="bysource-crownvalue">{fakeData[0].y}</h1>
+            </div>
+            <div className="flex">
+              <div className="legend brightorange"></div>
+              <div className="bysource-name"> {fakeData[0].x}</div>
+            </div>
+          </div>
+
+          <div className="flex">
+            <div className="bysource-row byintentborder">
+              <h1 className="bysource-value">{fakeData[1].y}</h1>
+              <div className="flex">
+                <div className="legend pink"></div>
+                <div className="byintent-name">{fakeData[1].x}</div>
+              </div>
+            </div>
+
+            <div className="bysource-row bysourceborder">
+              <h1 className="bysource-value">{fakeData[2].y}</h1>
+              <div className="flex">
+                <div className="legend lightpink"></div>
+                <div className="bysource-name">{fakeData[2].x}</div>
+              </div>
+            </div>
+          </div>
+          <div className="flex">
+            <div className="bysource-row">
+              <h1 className="bysource-value">{fakeData[3].y}</h1>
+              <div className="flex">
+                <div className="legend darkpurple"></div>
+                <div className="bysource-name">{fakeData[3].x}</div>
+              </div>
+            </div>
+
+            <div className="bysource-row">
+              <h1 className="bysource-value">{fakeData[4].y}</h1>
+              <div className="flex">
+                <div className="legend lilac"></div>
+                <div className="bysource-name">{fakeData[4].x}</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="flex">
-        <div className="bysource-row byintentborder">
-          <h1 className="bysource-value">{fakeData[1].y}</h1>
-          <div className="flex">
-            <div className="legend pink"></div>
-            <div className="byintent-name">{fakeData[1].x}</div>
-          </div>
-        </div>
-
-        <div className="bysource-row bysourceborder">
-          <h1 className="bysource-value">{fakeData[2].y}</h1>
-          <div className="flex">
-            <div className="legend lightpink"></div>
-            <div className="bysource-name">{fakeData[2].x}</div>
-          </div>
-        </div>
-      </div>
-      <div className="flex">
-        <div className="bysource-row">
-          <h1 className="bysource-value">{fakeData[3].y}</h1>
-          <div className="flex">
-            <div className="legend darkpurple"></div>
-            <div className="bysource-name">{fakeData[3].x}</div>
-          </div>
-        </div>
-
-        <div className="bysource-row">
-          <h1 className="bysource-value">{fakeData[4].y}</h1>
-          <div className="flex">
-            <div className="legend lilac"></div>
-            <div className="bysource-name">{fakeData[4].x}</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+    </Box>
+  </>
 );
 
 export default BySource;
--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,55 +1,66 @@
-import React, { useState } from 'react';
-import { VictoryLine, VictoryChart, VictoryTheme } from 'victory';
-import Box from './Box'
+import React, { useState } from "react";
+import { VictoryLine, VictoryChart, VictoryTheme } from "victory";
+import Box from "./Box";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import DonationAmount from "./DonationAmount";
-import KeyStatistics from "./KeyStatistics" ;
+import KeyStatistics from "./KeyStatistics";
 import BySource from "./BySource";
+import ByIntent from "./ByIntent";
 import "./index.css";
 
 const fakeData = [
-    { x: "cats", y: 55, fill: "#0a2" },
-    { x: "dogs", y: 124, fill: "blue" },
-    { x: "rabbits", y: 20, fill: "orange" }
-  ];
-  
-  const Dashboard  = () => { 
-    const [startDate, setStartDate] = useState(new Date("2019-11-20"));
-    const [endDate, setEndDate] = useState(new Date());
-    return (
-      <>
-        <div className = "dashboard-header wrap">
-          <h1>Dashboard</h1>
-            <div className ="d-flex">
-              <div>
-                <label htmlFor="startDate"> From &nbsp; {"      "}</label>
+  { x: "cats", y: 55, fill: "#0a2" },
+  { x: "dogs", y: 124, fill: "blue" },
+  { x: "rabbits", y: 20, fill: "orange" }
+];
 
-                <DatePicker id="startDate" selected={startDate} onChange={date => setStartDate(date)} selectsStart 
-                startDate={startDate} endDate={endDate}/>
-              </div>
-                <div>
-                  <label htmlFor="endDate"> To &nbsp; {"      "}</label>
-                  <DatePicker id="endDate" selected={endDate} onChange={date => setEndDate(date)}  selectsEnd endDate={endDate} minDate={startDate} />
-                </div>
-                <button className="button orangebutton ml-auto">View Donor List</button>
-            </div>
+const Dashboard = () => {
+  const [startDate, setStartDate] = useState(new Date("2019-11-20"));
+  const [endDate, setEndDate] = useState(new Date());
+  return (
+    <>
+      <div className="dashboard-header wrap">
+        <h1>Dashboard</h1>
+        <div className="d-flex">
+          <div>
+            <label htmlFor="startDate"> From &nbsp; {"      "}</label>
+
+            <DatePicker
+              id="startDate"
+              selected={startDate}
+              onChange={date => setStartDate(date)}
+              selectsStart
+              startDate={startDate}
+              endDate={endDate}
+            />
+          </div>
+          <div>
+            <label htmlFor="endDate"> To &nbsp; {"      "}</label>
+            <DatePicker
+              id="endDate"
+              selected={endDate}
+              onChange={date => setEndDate(date)}
+              selectsEnd
+              endDate={endDate}
+              minDate={startDate}
+            />
+          </div>
+          <button className="button orangebutton ml-auto">
+            View Donor List
+          </button>
         </div>
+      </div>
 
-        <div className = "wrap"> 
-            
+      <div className="wrap">
           <DonationAmount />
-
           <KeyStatistics />
-          
-          <BySource />    
-
-          <BySource />    
-          
-        </div>
-      </>
+          <BySource />
+          <ByIntent/>
         
-      );
-    }
-  
-  export default Dashboard;
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
