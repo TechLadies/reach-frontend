e5aa0d5d37e126cd793660892d965784425c537d
Ler Ka Leng <kalengzonline@hotmail.com>
2019-12-07 02:54:51 +0800

    styling of byIntent chart

--- src/components/Dashboard/ByIntent.js
+++ src/components/Dashboard/ByIntent.js
@@ -1,37 +1,45 @@
 import React, { useState } from 'react';
-import { VictoryBar, VictoryChart, VictoryTheme } from 'victory';
+import { VictoryBar, VictoryChart, VictoryAxis } from 'victory';
 import Box from './Box'
 
 const sampleData = [
     { x: "Counselling", y: 2320, fill: "#80485B" },
     { x: "Family", y: 823, fill: "#FFA001" },
-    { x: "Senior", y: 390, fill: "#FF9A85" },
+    { x: "Senior", y: 390, fill: "#80485B" },
     { x: "Youth", y: 402, fill: "#80485B" },
   ];
 
 const ByIntent  = () => {
     return (
         <div className="dashboard-header">
-            <h1 className="dashboard-headertxt">By Intent</h1>
+        <header className="dashboard-header">Donation Amount</header>
+         <h1 className= "dashboard-headertxt">By Intent</h1>
             
              <div className = "byintent-bar">
                 <Box>
-                <VictoryChart
+
+                <VictoryChart horizontal
                     domainPadding={{ x: 10 }}
                     height={265}
                     width={220}
+                    labelComponent={<VictoryAxis tickFormat={() => ""} />}
                     >
+                         
                     <VictoryBar horizontal
-                        style={{
-                        data: { fill: "#80485B" }
-                        }}
                         data={sampleData}
+                        labels={({ datum }) => ` ${datum.x}`}
+                        style={{
+                            data: {
+                              fill: ({ datum }) => datum.fill,
+                            }
+                          }}
+                        
                         
                     />
                     </VictoryChart>
                     </Box>
                 </div>
-
+        
 
         </div>
     )
