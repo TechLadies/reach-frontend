539f03984206b433de9aa0a5cdc4f529c845707f
chong lu khei <clukhei@gmail.com>
2019-12-11 17:34:57 +0800

    setup update donor database page, routers and added all elements without stylings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
 import Navbar from "./components/navbar";
 import Login from "./components/Login";
 import Dummy from "./components/Dummy";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
+import UpdateDb from "./components/UpdateDB";
 
 function App() {
   return (
@@ -17,8 +18,11 @@ function App() {
           <Route path="/dummy">
             <Dummy />
           </Route>
-          <Route path="/">
+          <Route path="/update_donor_database">
             <Navbar />
+            <UpdateDb />
+          </Route>
+          <Route path="/">
             <Dashboard />
           </Route>
         </Switch>
---
+++ src/components/UpdateDB/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Box from "../../components/Dashboard/Box";
+import UpdateDbImg from "../../images/updatedonordb.svg";
+import Reportplus from '../../images/reportplus.svg';
+
+const UpdateDb = () => {
+  return (
+    <Box>
+      <img src={UpdateDbImg} alt="Update donor database" />
+      <div>
+        <div>Last database update</div>
+        <div>For donations in the period of </div>
+      </div>
+      <div>
+        <h1>To update the database upload the IPC file here</h1>
+        <div>
+          <button className="button orangebutton">
+            <img src={Reportplus} className="button-icon" alt ="Upload IPC file"/>
+            Upload IPC File
+          </button>
+        </div>
+      </div>
+    </Box>
+  );
+};
+
+export default UpdateDb;
