ae88f8e6265bdbb73c2d27bd3bcc595e242be370
Ler Ka Leng <kalengzonline@hotmail.com>
2019-12-24 01:07:09 +0800

    Keystats CSS Changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
 import Navbar from "./components/navbar";
 import Login from "./components/Login";
 import Dummy from "./components/Dummy";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 
 function App() {
@@ -17,13 +17,25 @@ function App() {
           <Route path="/dummy">
             <Dummy />
           </Route>
-          <Route path="/">
+          <ProtectedRoute path="/">
             <Navbar />
             <Dashboard />
-          </Route>
+          </ProtectedRoute>
         </Switch>
       </BrowserRouter>
     </div>
+  );
+}
+
+function ProtectedRoute({ children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        if (!localStorage.getItem("token")) return <Redirect to="/login" />;
+        return children;
+      }}
+    />
   );
 }
 
