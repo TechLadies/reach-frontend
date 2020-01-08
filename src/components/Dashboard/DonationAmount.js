646a0ac24b2baf138365f7b71d822504eab55557
chong lu khei <clukhei@gmail.com>
2020-01-09 00:12:08 +0800

    convert apidata amount to integer value, passed into line graph

--- src/components/Dashboard/DonationAmount.js
+++ src/components/Dashboard/DonationAmount.js
@@ -1,34 +1,57 @@
 import React from 'react'
-import { VictoryLine, VictoryChart, VictoryAxis } from 'victory'
+import {
+  VictoryLine,
+  VictoryChart,
+  VictoryAxis,
+  LineSegment,
+  VictoryTooltip,
+} from 'victory'
 import Box from './Box'
 import theme from './VictoryTheme'
 
 const DonationAmount = props => {
-  const donationsArr= props.data.donationAmt
+  if (!DonationAmount) return null
+  const donationsArr = props.data.donationAmt
   const transformDonation = donationsArr => {
-    return {date: new Date (donationsArr.date), amount: donationsArr.amount}
+    return { x: new Date(donationsArr.date), y: Number(donationsArr.amount) }
   }
-  const newDonationsArr = donationsArr.map(e=> transformDonation(e))
-  
+  const newDonationsArr = donationsArr.map(e => transformDonation(e))
+
   console.log(newDonationsArr)
   return (
     <div className="dashboard-gridcontent">
       <h1 className="dashboard-headertxt">Donation Amount</h1>
       <Box>
         <div className="line-chart">
-          <VictoryChart theme={theme} height={200} width={300}> 
+          <VictoryChart
+            data={newDonationsArr}
+            theme={theme}
+            scale={{ x: 'time' }} /* maxDomain = {{y: 3000}} */
+          >
+            <VictoryAxis
+              crossAxis
+              dependentAxis
+              tickFormat={y => {
+                if (y >= 1000) {
+                  return y / 1000 + 'k'
+                }
+              }}
+              label={'Amount $'}
+              gridComponent={<LineSegment type={"grid"}/>}
+              /*  grid = */ /* {
+                {stroke: ({tick}) =>  tick === 0 ? "black" : "#A9A9A9",
+                strokeWidth: 4}  
+               } */
+            />
+            <VictoryAxis crossAxis />
             <VictoryLine
+              interpolation="linear"
               style={{
                 data: { stroke: '#FE5366' },
-                parent: { border: '1px solid #CC7503' },
-                tickLabels: {fontSize: 5}
+                parent: { border: '1px solid #CC7503' }
               }}
               data={newDonationsArr}
-              y="amount"
-              x="date"
-              
             />
-            {/* <VictoryAxis style ={{tickLabels: {fontSize:5}}}/> */}
           </VictoryChart>
         </div>
       </Box>
