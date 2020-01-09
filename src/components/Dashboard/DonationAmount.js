235df25025644e18ca5da8929bb0a979c1c8ef57
chong lu khei <clukhei@gmail.com>
2020-01-09 23:27:04 +0800

    styling of linegraph in progress

--- src/components/Dashboard/DonationAmount.js
+++ src/components/Dashboard/DonationAmount.js
@@ -5,6 +5,7 @@ import {
   VictoryAxis,
   LineSegment,
   VictoryTooltip,
+  VictoryLabel
 } from 'victory'
 import Box from './Box'
 import theme from './VictoryTheme'
@@ -17,10 +18,12 @@ const DonationAmount = props => {
   }
   const newDonationsArr = donationsArr.map(e => transformDonation(e))
 
-  const yValues= newDonationsArr.map(e=> e.y).sort(function(a,b){return a-b})
+  const yValues= newDonationsArr.map(e=> e.y)/* .sort(function(a,b){return a-b}) */
   const maxY= Math.ceil((Math.max(...yValues))/1000)*1000
   const minY= Math.floor((Math.min(...yValues))/1000)*1000
   const middleY = Math.ceil(((maxY + minY)/2)/1000)*1000
+ /*  const q1Y= Math.floor(((middleY+minY)/2)/1000)*1000
+  const q3Y= Math.floor(((middleY+maxY)/2)/1000*1000 */
   /* const midY= Math.ceil((yValues[Math.floor(yValues.length/2)])/1000)*1000
   const meanY= Math.ceil(((yValues.reduce((a,b)=> a + b, 0))/yValues.length)/1000)*1000 */
 
@@ -41,6 +44,7 @@ const DonationAmount = props => {
               crossAxis
               dependentAxis
               tickValues={yAxisTicks}
+              
               tickFormat={y => {
                 if (y >= 1000) {
                   return y / 1000 + 'k'
@@ -49,17 +53,19 @@ const DonationAmount = props => {
               label={'Amount $'}
               style = {{
                 grid: {stroke: ({ tick }) => "#EBEDF0"},
-                 ticks: {stroke: "grey", size: 5},
-      
+                 axis: {stroke: "none"},
+                 tick: '#9FA2B4',
+                 tickLabels: {fontSize: 10, fill: '#9FA2B4', "opacity": "0.5", verticalAnchor: 'start'},
+                 label : {angle: -45}
               }}
              
             />
-            <VictoryAxis crossAxis />
+            <VictoryAxis crossAxis style= {{axis: {stroke: "#CC7503" ,"opacity": "0.5", strokeWidth: 1.5}}} />
             <VictoryLine
               interpolation="linear"
               style={{
-                data: { stroke: '#FE5366' },
-                parent: { border: '1px solid #CC7503' }
+                data: { stroke: '#FE5366', strokeWidth: 1.5 },
+                
               }}
               data={newDonationsArr}
             />
