36c9695bb43899f1fb79a9dd3c7f8dae764b2c48
Ler Ka Leng <kalengzonline@hotmail.com>
2020-01-15 00:50:28 +0800

    resolve merge conflicts from master

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -91,39 +91,39 @@ const Dashboard = () => {
     <>
       <Header>
         <Header.Top>
-          <Header.Content>Dashboard</Header.Content>
-        </Header.Top>
-        <Header.Bottom>
-          <div className="d-flex">
-            <div>
-              <label className="datelabel-from" htmlFor="startDate">
-                {" "}
-                From &nbsp; {"      "}
-              </label>
-              <DatePicker
-                className="dateform"
-                selected={startDate}
-                onChange={date => setStartDate(date)}
-                selectsStart
-                startDate={startDate}
-                endDate={endDate}
-              />
+          <Header.Content>
+            <h2>Dashboard</h2>
+            <div className="d-flex">
+              <div>
+                <label className="datelabel-from" htmlFor="startDate">
+                  {" "}
+                  From &nbsp; {"      "}
+                </label>
+                <DatePicker
+                  className="dateform"
+                  selected={startDate}
+                  onChange={date => setStartDate(date)}
+                  selectsStart
+                  startDate={startDate}
+                  endDate={endDate}
+                />
+              </div>
+              <div>
+                <label className="datelabel-to" htmlFor="endDate">
+                  {" "}
+                  To &nbsp; {"      "}
+                </label>
+                <DatePicker
+                  className="dateform"
+                  selected={endDate}
+                  onChange={date => setEndDate(date)}
+                  selectsEnd
+                  endDate={endDate}
+                  minDate={startDate}
+                />
+              </div>
             </div>
-            <div>
-              <label className="datelabel-to" htmlFor="endDate">
-                {" "}
-                To &nbsp; {"      "}
-              </label>
-              <DatePicker
-                className="dateform"
-                selected={endDate}
-                onChange={date => setEndDate(date)}
-                selectsEnd
-                endDate={endDate}
-                minDate={startDate}
-              />
-            </div>
-          </div>
+          </Header.Content>
           <Header.Buttons>
             <a
               className="button orangebutton donor-list-link"
@@ -133,7 +133,7 @@ const Dashboard = () => {
               View Donor List
             </a>
           </Header.Buttons>
-        </Header.Bottom>
+        </Header.Top>
       </Header>
 
       <div className="wrap">
