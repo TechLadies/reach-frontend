2b97e867b80cd25ead78cc0cf3d64213ea86992b
chong lu khei <clukhei@gmail.com>
2020-01-22 22:23:12 +0800

    apply bootstrap tabs and tables to successful upload page (WIP)

--- src/components/UpdateDB/Success.js
+++ src/components/UpdateDB/Success.js
@@ -4,12 +4,13 @@ import TwoHands from '../../images/two-hands-donation.svg'
 import DonationCircle from '../../images/donations-circle.svg'
 import Calendar from '../../images/calendar-circle.svg'
 import Box from '../../components/Dashboard/Box'
+import { Tabs, Tab } from 'react-bootstrap'
 
 const SuccessUpload = () => {
   return (
-    <div className= "success-container">
-      <header className= "success-header">File Uploaded Successfully!</header>
-      <img src={HiFive} alt="success" className="hifive-img"/>
+    <div className="success-container">
+      <header className="success-header">File Uploaded Successfully!</header>
+      <img src={HiFive} alt="success" className="hifive-img" />
       <Box className="summary-container">
         <div className="summary-subcontainer">
           <img src={TwoHands} className="success-img" alt="twohands" />
@@ -33,8 +34,95 @@ const SuccessUpload = () => {
           </div>
         </div>
       </Box>
+      <div className="tab">
+        <Tabs
+          defaultActiveKey="all"
+          id="uncontrolled-tab-example"
+          className= "nav nav-tabs nav-justified"
+        >
+          <Tab eventKey="all" title={<div>All Donors</div>}>
+            <Table />
+          </Tab>
+          <Tab eventKey="new" title={<div>New Donors</div>}>
+            <Table />
+          </Tab>
+          <Tab eventKey="existing" title={<div>Existing Donors</div>}>
+            <Table />
+          </Tab>
+        </Tabs>
+      </div>
     </div>
   )
 }
 
 export default SuccessUpload
+
+const Table = () => {
+  const data = {
+    donationList: [
+      {
+        IDNumber: 'S1234567B',
+        name: 'Alan Tan',
+        totalAmountDonated: '$56,900',
+        noOfDonations: '1',
+        donorType: 'New',
+        viewProfile: ''
+      },
+      {
+        IDNumber: 'S1234567B',
+        name: 'Alan Tan',
+        totalAmountDonated: '$56,900',
+        noOfDonations: '1',
+        donorType: 'New',
+        viewProfile: ''
+      },
+      {
+        IDNumber: 'S1234567B',
+        name: 'Alan Tan',
+        totalAmountDonated: '$56,900',
+        noOfDonations: '1',
+        donorType: 'New',
+        viewProfile: ''
+      }
+    ]
+  }
+  return (
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col" width="120px">ID Number</th>
+          <th scope="col" width= "200px">Name</th>
+          <th scope="col" width= "127px">Total Amt Donated</th>
+          <th scope="col" width= "105px">No. of Donations</th>
+          <th scope="col" width= "75px">Donor Type</th>
+          <th scope="col" width= "110px">View Profile </th>
+        </tr>
+      </thead>
+      <tbody>
+        {' '}
+        <ListItem data={data.donationList} />{' '}
+      </tbody>
+    </table>
+  )
+}
+
+function ListItem(props) {
+  let listElements = props.data
+
+  let listComponents = listElements.map(item => {
+    return (
+      <tr>
+        <td> {item.IDNumber} </td>
+        <td>{item.name}</td>
+        <td>{item.totalAmountDonated}</td>
+        <td>{item.noOfDonations}</td>
+        <td>{item.donorType}</td>
+        <td>
+          <a href="{item.viewProfile}">View Profile</a>
+        </td>
+      </tr>
+    )
+  })
+
+  return <React.Fragment>{listComponents}</React.Fragment>
+}
