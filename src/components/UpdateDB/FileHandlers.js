761d8875e915802719cb72d9b2becf9c9d1c2982
chong lu khei <clukhei@gmail.com>
2020-03-13 18:14:35 +0800

    configure receipt no. to be uploaded as string, according to db datatype for receipt no.

--- src/components/UpdateDB/FileHandlers.js
+++ src/components/UpdateDB/FileHandlers.js
@@ -1,29 +1,39 @@
-import React from "react";
-import Reportplus from "../../images/reportplus.svg";
-import "./index.css";
-import * as Papa from "papaparse";
-
-
+import React from 'react'
+import Reportplus from '../../images/reportplus.svg'
+import './index.css'
+import * as Papa from 'papaparse'
 
 function FileHandlers(props) {
-  const onCancel= () =>{
+  const onCancel = () => {
     props.CPU()
   }
   const handleSelectedFile = e => {
-    console.log(e.target.files[0]);
+    console.log(e.target.files[0])
 
     Papa.parse(e.target.files[0], {
       header: true,
       download: false,
-      delimiter: ",",
+      delimiter: ',',
       skipEmptyLines: true,
       dynamicTyping: true,
-      complete: function(results) {  
-        props.loadIpcEntries(results.data);
-        console.log(results.data);
+      complete: function(results) {
+        const processedArray = results.data.map(function(e) {
+
+          if ( typeof e['Receipt Serial No'] === 'string') {
+            return {...e}
+          } else {
+            return {
+              ...e,
+              'Receipt Serial No' : e['Receipt Serial No'].toString()
+            }
+          }
+        })
+        console.log(processedArray)      
+        return props.loadIpcEntries(processedArray)
+       
       }
-    });
-  };
+    })
+  }
 
   return (
     <div>
@@ -33,15 +43,15 @@ function FileHandlers(props) {
         id="file"
         onChange={handleSelectedFile}
         accept=".csv"
-        value= {onCancel}
-        className= "defaultinputfile"
+        value={onCancel}
+        className="defaultinputfile"
       />
-      <label for= "file" className= "button orangebutton">
-      <img src={Reportplus} className="button-icon" alt="Upload IPC file" />
+      <label for="file" className="button orangebutton">
+        <img src={Reportplus} className="button-icon" alt="Upload IPC file" />
         Upload IPC File
       </label>
     </div>
-  );
+  )
 }
 
-export default FileHandlers;
+export default FileHandlers
