efdbc39bf2b999c4a2056e9febec01e590728105
chong lu khei <clukhei@gmail.com>
2020-04-02 23:55:42 +0800

    refactor dates
    let FE handle date format change(previously done on BE) from dd/mm/yyyy to yyyy-mm-dd for FILE UPLOAD feature
    
    rename dateStringGenerator.js to date.js and move all date related functions into date.js
    
    let FE handle dateformat presentation for upload success summary (previously done on BE)

---
+++ src/lib/date.js
@@ -0,0 +1,54 @@
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+
+function dateStringOf(date) {
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const month = months[date.getMonth()];
+    const printDate = day + " " + month + " " + year;
+  
+    return printDate;
+  }
+
+  function dateVariation(date) {
+    const day = date.getDate()
+    const year = date.getFullYear()
+    const month = months[date.getMonth()]
+    const fullDate = day + ' ' + month + ' ' + year
+    const dateMonth = day + ' ' + month
+  
+    return { day, month, year, fullDate, dateMonth }
+  }
+  
+  const periodFormatter = (minDate, maxDate) => {
+    if (minDate.year === maxDate.year) {
+      if (minDate.month === maxDate.month) {
+        return minDate.day + '-' + maxDate.day + maxDate.month + maxDate.year
+      } else {
+        return minDate.dateMonth + '-' + maxDate.dateMonth + maxDate.year
+      }
+    } else {
+      return minDate.fullDate + '-' + maxDate.fullDate
+    }
+  }
+  
+  const reformatDate = date => {
+    return date
+      .split('/')
+      .reverse()
+      .join('-')
+  }
+
+  export {dateStringOf , dateVariation, periodFormatter, reformatDate};
