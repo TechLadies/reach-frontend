29b33eccb9a467dee3aa3cdbabcdeaebd0656d03
chong lu khei <clukhei@gmail.com>
2020-04-14 23:02:40 +0800

    liveshare: pradnya fetched code

--- src/components/UpdateDB/index.js
+++ src/components/UpdateDB/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Box from '../../components/Dashboard/Box'
 import UpdateDbImg from '../../images/updatedonordb.svg'
 import './index.css'
@@ -7,60 +7,31 @@ import ConfirmUpload from './ConfirmUpload'
 import ProgressBar from './ProgressBar'
 import SuccessUpload from './Success'
 import FailedImg from '../../images/uploadfail.svg'
-import {dateStringOf , dateVariation, periodFormatter, reformatDate} from '../../lib/date.js'
-
-const getLatestUpload =  () => {
-  return fetch(
-    `${process.env.REACT_APP_API}/uploads/latest,
-    {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' }
-    }
-  )
- .then(resp => resp.json())
- .catch(err => {
-   console.log("err:  ",JSON.stringify(err)));
- });  
-};
-
-function LatestUpdates(props) {
-  const [id,setId] = useState();
-  const [firstUpdate,setFirstUpdate] =useState() /*:period: '1 Sep 2019 - 31 Oct 2019'*/
-  const [lastUpdate, setLastUpdate] =useState() /*: '16 Sep 2019, 13:94',*/
-  const [updatedAt,setUpdatedAt]=useState();
-}
-useEffect(()=> {
-  getLatestUpload().then(result=> {
-    console.log(result);
-    setId(result.data);
-    setFirstUpdate(result.data);
-  });
-  []);
-})
-
-// const fakeUpdates = {
-//   lastUpdate: '16 Sep 2019, 13:94',
-//   period: '1 Sep 2019 - 31 Oct 2019'
-// } 
+import {
+  dateStringOf,
+  dateVariation,
+  periodFormatter,
+  reformatDate,
+} from '../../lib/date.js'
 
 const priorUploadState = {
   showPopUp: false,
   ipcData: [],
   uploading: false,
   failedUpload: false,
-  successUpload: null
+  successUpload: null,
 }
 
 const UpdateDb = () => {
   const [upload, setUpload] = useState(priorUploadState)
 
-  const loadIpcEntries = entries => {
+  const loadIpcEntries = (entries) => {
     setUpload({
       showPopUp: true,
       ipcData: entries,
       uploading: false,
       failedUpload: false,
-      successUpload: null
+      successUpload: null,
     })
   }
 
@@ -70,7 +41,7 @@ const UpdateDb = () => {
       ipcData: [],
       uploading: false,
       failedUpload: false,
-      successUpload: null
+      successUpload: null,
     })
   }
 
@@ -80,26 +51,24 @@ const UpdateDb = () => {
       ipcData: upload.ipcData,
       uploading: true,
       failedUpload: false,
-      successUpload: null
+      successUpload: null,
     })
 
-    const validateUpSert = res => {
+    const validateUpSert = (res) => {
       if (res.ok) {
-        res
-          .json()
-          .then(data => success(data))
+        res.json().then((data) => success(data))
       } else {
         return failed()
       }
     }
-    
+
     fetch(`${process.env.REACT_APP_API}/donations/upload`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(upload.ipcData)
+      body: JSON.stringify(upload.ipcData),
     })
       .then(validateUpSert)
-      .catch(err => console.log(err))
+      .catch((err) => console.log(err))
   }
 
   const failed = () => {
@@ -108,81 +77,110 @@ const UpdateDb = () => {
       ipcData: [],
       uploading: false,
       failedUpload: true,
-      successUpload: null
+      successUpload: null,
     })
   }
-  const success = data => {
+  const success = (data) => {
     setUpload({
       showPopUp: false,
       ipcData: [],
       uploading: false,
       failedUpload: false,
-      successUpload: data
+      successUpload: data,
     })
   }
 
   return (
-    <div > 
+    <div>
       {upload.successUpload ? (
         <SuccessUpload donorData={upload.successUpload} />
       ) : (
-        <div className ="updatedb-wrapper">
-        <Box className="updatedb-box">
-          {upload.failedUpload ? (
-            <img
-              src={FailedImg}
-              alt="oops, an error occurred"
-              className="failimg"
-            />
-          ) : (
-            <img
-              src={UpdateDbImg}
-              alt="Update donor database"
-              className="uploadimg"
-            />
-          
-          )}
-          
-          <div className="updatedetails-container">
-            {upload.failedUpload ? <FailMsg /> : <UploadMsg />}
-            <FileHandlers loadIpcEntries={loadIpcEntries} CPU={cancelPopUp} />
-          </div>
-        
-          
-          {upload.showPopUp && (
-            <ConfirmUpload
-              CPU={cancelPopUp}
-              ipcEntries={upload.ipcData}
-              clickYes={onYesContinue}
-              success={success}
-              failed={failed}
-            />
-          )}
-          {upload.uploading && (
-            <ProgressBar onFailedUpload={failed} progress={upload.percentage} />
-          )}
-        </Box>
+        <div className="updatedb-wrapper">
+          <Box className="updatedb-box">
+            {upload.failedUpload ? (
+              <img
+                src={FailedImg}
+                alt="oops, an error occurred"
+                className="failimg"
+              />
+            ) : (
+              <img
+                src={UpdateDbImg}
+                alt="Update donor database"
+                className="uploadimg"
+              />
+            )}
+
+            <div className="updatedetails-container">
+              {upload.failedUpload ? <FailMsg /> : <UploadMsg />}
+              <FileHandlers loadIpcEntries={loadIpcEntries} CPU={cancelPopUp} />
+            </div>
+
+            {upload.showPopUp && (
+              <ConfirmUpload
+                CPU={cancelPopUp}
+                ipcEntries={upload.ipcData}
+                clickYes={onYesContinue}
+                success={success}
+                failed={failed}
+              />
+            )}
+            {upload.uploading && (
+              <ProgressBar
+                onFailedUpload={failed}
+                progress={upload.percentage}
+              />
+            )}
+          </Box>
         </div>
       )}
     </div>
-    
   )
 }
 
+const getLatestUpload = async () => {
+  return await fetch(`${process.env.REACT_APP_API}/uploads/latest`, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  })
+    .then((resp) => resp.json())
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+
+// const fakeUpdates = {
+//   lastUpdate: 'latestUpload.createdAt,
+//   period: 'latestUpload.firstUpdate -latestUpload.LastUpdate 1 Sep 2019 - 31 Oct 2019'
+// }
+
 const UploadMsg = () => {
+  const [latestUpload, setLatestUpload] = useState({})
+  useEffect(() => {
+    getLatestUpload().then((result) => {
+      setLatestUpload(result)
+    })
+  }, [])
+
+ /* 1) try to split 2018-01-21 T00:00:00.000Z to get date and time 
+    2) use dateStringOf(date) to get date format
+    3) create a function to format the time (if required)
+    4) apply the time formatting function to the time  */
+ 
   return (
-    <div >
+    <div>
       <div className=" update-top">
         <div className="container1">
           <h1 className="grey-header">Last database update</h1>
           <div className="container2">
-            <p className=" update-data">{fakeUpdates.lastUpdate}</p>
+            <p className=" update-data">{latestUpload.createdAt}</p>
           </div>
         </div>
         <div className="container1">
           <h1 className="grey-header">For donations in the period of </h1>
           <div className="container2">
-            <p className="update-data"> {fakeUpdates.period} </p>
+            <p className="update-data"> {latestUpload.firstDate} - {latestUpload.lastDate}</p>
           </div>
         </div>
       </div>
