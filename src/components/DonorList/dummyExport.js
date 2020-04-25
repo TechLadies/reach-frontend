2e383829df4f877b4dac170104f918e473f89a5a
psjoshi20 <psjoshi20@gmail.com>
2020-04-25 22:31:56 +0800

    WIP jsoo to csv

--- src/components/DonorList/dummyExport.js
+++ src/components/DonorList/dummyExport.js
@@ -4,31 +4,35 @@ import * as Papa from 'papaparse';
 // const convertJSONToCSV = () => {
   function convertJSONToCSV(props) {
     const csv = Papa.unparse({
-        fields: ["IdNo", "name", "contactNo", "email", "dnc"],
+        fields: ["IdNo", "name","amount Donated", "contactNo", "email", "dnc"],
         data: [{
-            idNo: "S409382F",
+         idNo: "S409382F",
          name: "Keyboard Warrior",
+         amount Donated:350,
          contactNo: null, 
-         email: "",
+         email: "tkneo@gmail.com",
          dnc: true
           },
           {
                             idNo: "S9209230H",
                              name: "Sylvica Chan",
+                             amount Donated:33.5,
                               contactNo: null, 
-                              email: "", 
+                              email: "tkneo@gmail.com", 
                               dnc: null
                         },
                         {
                             idNo: "S23231232A",
                              name: "TowKay Neo", 
+                             amount Donated:98734923,
                              contactNo: "829212412",
                               email: "tkneo@gmail.com",
                                dnc: true
                         },
                     {
                         idNo: "8293270", 
-                        name: "MediaWks", 
+                        name: "MediaWks",
+                        amount Donated:110,
                         contactNo: "82938101", 
                         email: "mediaworks@gmail.com", 
                         dnc: null 
