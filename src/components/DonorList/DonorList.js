5fd02736a1cc37c81fc55e1128bab242883a3d28
Lu Khei <kheiboardwarrior@gmail.com>
2020-05-14 00:49:44 +0800

    apply filters and close modal, reset filters for UI only

--- src/components/DonorList/DonorList.js
+++ src/components/DonorList/DonorList.js
@@ -28,10 +28,7 @@ function DonorList() {
   const [donorsPerPage] = useState(10);
   const [donorCount, setDonorCount] = useState(0);
   const [query, setQuery] = useState(``);
-
-
-
-
+  const [res, setRes] = useState(false);
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   useEffect(() => {
@@ -41,35 +38,6 @@ function DonorList() {
     });
   }, [query]);
   console.log(donorList);
-
-  /* function handlePageChange(number) {
-    setCurrentPage(number);
-    getDonorData(number).then((result) => {
-      setDonorList(result);
-    });
-  }
- */
-  function ListItem(props) {
-    let listElements = props.data;
-    let listComponents = listElements.map((item, i) => {
-      return (
-        <tr
-          key={i}
-          className={item.idNo ? "donorlink" : ""}
-          onClick={item.idNo && (() => history.push(`/details/${item.idNo}`))}
-        >
-          <td scope="row"> {item.idNo} </td>
-          <td>{item.name}</td>
-          <td>{item.totalAmountDonated}</td>
-          <td>{item.contactNo}</td>
-          <td>{item.email}</td>
-          <td>{item.dnc ? "Do Not Contact" : "Can Contact"}</td>
-        </tr>
-      );
-    });
-
-    return <React.Fragment>{listComponents}</React.Fragment>;
-  }
 
   return (
     <div class="Donor Table">
@@ -112,23 +80,9 @@ function DonorList() {
       )}
 
       {
-        /* Array.isArray(donationList) && */ donorList.length > 0 ? (
+        /* Array.isArray(donationList) && */ donorList.length >= 0 ? (
           // { donationList.length > 0 ? (
-          <table class="table donortable">
-            <thead>
-              <tr>
-                <th scope="col">ID Number</th>
-                <th scope="col">Name</th>
-                <th scope="col">Amount Donated</th>
-                <th scope="col">Contact Number</th>
-                <th scope="col">Email Address</th>
-                <th scope="col">DNC Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              <ListItem data={donorList} />
-            </tbody>
-          </table>
+          <DonorListTable data={donorList} />
         ) : (
           <Spin />
         )
@@ -146,6 +100,52 @@ function DonorList() {
       </div>
     </div>
   );
+}
+
+const DonorListTable = (props) => {
+  const donorList = props.data;
+
+  return donorList.length > 0 ? (
+    <table class="table donortable">
+      <thead>
+        <tr>
+          <th scope="col">ID Number</th>
+          <th scope="col">Name</th>
+          <th scope="col">Amount Donated</th>
+          <th scope="col">Contact Number</th>
+          <th scope="col">Email Address</th>
+          <th scope="col">DNC Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <ListItem data={donorList} />
+      </tbody>
+    </table>
+  ) : (
+    null
+    /* <div className= "no-donor">No donors found</div> */
+  );
+};
+function ListItem(props) {
+  let listElements = props.data;
+  let listComponents = listElements.map((item, i) => {
+    return (
+      <tr
+        key={i}
+        className={item.idNo ? "donorlink" : ""}
+        onClick={item.idNo && (() => history.push(`/details/${item.idNo}`))}
+      >
+        <td scope="row"> {item.idNo} </td>
+        <td>{item.name}</td>
+        <td>{item.totalAmountDonated}</td>
+        <td>{item.contactNo}</td>
+        <td>{item.email}</td>
+        <td>{item.dnc ? "Do Not Contact" : "Can Contact"}</td>
+      </tr>
+    );
+  });
+
+  return <React.Fragment>{listComponents}</React.Fragment>;
 }
 
 export default DonorList;
--- src/components/DonorList/filterPopup.js
+++ src/components/DonorList/filterPopup.js
@@ -10,13 +10,15 @@ const fetchSourceList = async () => {
   const data = res.json();
   return data;
 };
+const ref = React.createRef();
 
 function FilterPopUp(props) {
+  console.log(props.resStatus)
+ 
   const query = props.query;
-  const [filter, setFilter] = useState({source:[]});
-  console.log(filter);
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
+  const [filter, setFilter] = useState({ source: [] });
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
 
   const [sources, setSources] = useState([]);
   const [selectTypeAhead, setSelectedTypeAhead] = useState([]);
@@ -25,229 +27,235 @@ function FilterPopUp(props) {
     fetchSourceList().then((data) => setSources(data));
   }, []);
 
-  const buildQuery = () => {
-    var urlParams = []
-    for (let src of filter.source){
-      urlParams.push('source=' + src);
-    } 
+  const buildAndSubmitQuery = () => {
+    var urlParams = [];
+    for (let src of filter.source) {
+      urlParams.push("source=" + src);
+    }
 
     for (var key in filter) {
-      if(key === 'source'){
-        continue
+      if (key === "source") {
+        continue;
       }
-      urlParams.push(key + "=" + filter[key]);
+      if(filter[key]){
+        urlParams.push(key + "=" + filter[key]);
+      } 
     }
-    console.log(urlParams.join('&'));
-    props.setQuery(`?${urlParams.join('&')}`);
-  }
+    console.log(urlParams.join("&"));
+    props.setQuery(`?${urlParams.join("&")}`);
+    props.close()
+    
+  };
 
- 
   return (
     <Modal show={true} onHide={props.close} dialogClassName="modal-90w">
-      <div class="donorlist-modal">
-        <Modal.Header>
-          <div>
-            <h3 className="donorfilters-head">Donor Filters</h3>
-            <span className="donorfilters-subhead">
-              Donor has made at least 1 donation that satisfies the following
-              criteria
-            </span>
-          </div>
-
-          <Modal.Close onClick={props.close} />
-        </Modal.Header>
-        <Modal.Body>
-          <div className="modalbody">
-            <div className="modalfilter">
-              <b class="filterlabel">Date Range</b>
-              <div className="filterdatepicker d-flex">
-                <div>
-                  <label className="datelabel-from" htmlFor="startDate">
-                    {" "}
-                    From &nbsp; {"      "}
-                  </label>
-                  <DatePicker
-                    className="dateform"
-                    selected={startDate}
-                    name="from"
-                    onChange={(date) => {
-                      setStartDate(date);
-                      setFilter({ ...filter, from: date.toISOString() });
-                    }}
-                    selectsStart
-                    startDate={startDate}
-                    endDate={endDate}
-                    dateFormat="dd/MM/yyyy"
-                  />
-                </div>
-                <div>
-                  <label className="datelabel-to" htmlFor="endDate">
-                    {" "}
-                    To &nbsp; {"      "}
-                  </label>
-                  <DatePicker
-                    className="dateform"
-                    selected={endDate}
-                    name="to"
-                    onChange={(date) => {
-                      setEndDate(date);
-                      setFilter({ ...filter, to: date.toISOString() });
-                    }}
-                    selectsEnd
-                    endDate={endDate}
-                    minDate={startDate}
-                    dateFormat="dd/MM/yyyy"
-                  />
-                </div>
-              </div>
-            </div>
-
-            <div className="modalfilter">
-              <strong class="filterlabel">Tax Deductable Status</strong>
-
-              <div class="custom-control custom-radio custom-control-inline">
-                <input
-                  type="radio"
-                  class="custom-control-input"
-                  id="defaultInline1"
-                  name="taxDeduc"
-                  onChange={() =>
-                    "taxDeduc" in filter ? delete filter.taxDeduc : null
-                  }
-                />
-                <label class="custom-control-label" htmlFor="defaultInline1">
-                  Any
-                </label>
-              </div>
-
-              <div class="custom-control custom-radio custom-control-inline">
-                <input
-                  type="radio"
-                  class="custom-control-input"
-                  id="defaultInline2"
-                  name="taxDeduc"
-                  onChange={(e) =>
-                    setFilter({ ...filter, [e.target.name]: e.target.value })
-                  }
-                  value={true}
-                />
-                <label class="custom-control-label" htmlFor="defaultInline2">
-                  Tax Deductible
-                </label>
-              </div>
-
-              <div class="custom-control custom-radio custom-control-inline">
-                <input
-                  type="radio"
-                  class="custom-control-input"
-                  id="defaultInline3"
-                  name="taxDeduc"
-                  onChange={(e) =>
-                    setFilter({ ...filter, [e.target.name]: e.target.value })
-                  }
-                  value={false}
-                />
-                <label class="custom-control-label" htmlFor="defaultInline3">
-                  Non Tax Deductible
-                </label>
-              </div>
-            </div>
-
-            <div className="modalfilter">
-              <b class="filterlabel">Source contains any of these phrase(s)</b>
-              <span className="donorfilter-subhead">
-                Type in each source and press "Enter"
+      <form onSubmit={buildAndSubmitQuery}>
+        <div class="donorlist-modal">
+          <Modal.Header>
+            <div>
+              <h3 className="donorfilters-head">Donor Filters</h3>
+              <span className="donorfilters-subhead">
+                Donor has made at least 1 donation that satisfies the following
+                criteria
               </span>
-              <form
-                action=""
-                className="form-inline my-2 my-lg-0"
-                id="sourceSearchForm"
-              >
-                <Typeahead
-                  id="sources-list"
-                  labelKey="description"
-                  multiple={true}
-                  options={sources}
-                  name = "source[]"
-                  onChange={(selected) => {
-                    setSelectedTypeAhead(selected);
-                    setFilter({
-                      ...filter,
-                      source: selected.map((e) => {
-                        return e.description;
-                      }) 
-                    });
-                  }}
-                  placeholder="e.g. Charity Dinner, Reach website"
-                  selected={selectTypeAhead}
-                  className="sources"
-                />
-              </form>
             </div>
 
-            <div className="modalfilter">
-              <b class="filterlabel d-flex">Total Donated Amount</b>
-              <form
-                class="form-inline my-2 my-lg-0"
-                id="donationAmtSearchForm "
-              >
-                <input
-                  className="form-control mr-sm-2 form-amt"
-                  name="minAmt"
-                  type="number"
-                  step="0.01"
-                  placeholder="$.0.00"
-                  aria-label="Search"
-                  onChange={(e) => {
-                    setFilter({ ...filter, [e.target.name]: e.target.value });
-                    /* if (!filter.minAmt) delete filter.minAmt */
-                  }}
-                />
-                to&nbsp; {"      "}
-                <input
-                  className="form-control mr-sm-2 form-amt"
-                  name="maxAmt"
-                  type="number"
-                  step= "0.01"
-                  placeholder="$0.00"
-                  aria-label="Search"
-                  onChange={(e) =>
-                    setFilter({ ...filter, [e.target.name]: e.target.value })
-                  }
-                />
-              </form>
+            <Modal.Close onClick={props.close} />
+          </Modal.Header>
+          <Modal.Body>
+            <div className="modalbody">
+              <div className="modalfilter">
+                <b class="filterlabel">Date Range</b>
+                <div className="filterdatepicker d-flex">
+                  <div>
+                    <label className="datelabel-from" htmlFor="startDate">
+                      {" "}
+                      From &nbsp; {"      "}
+                    </label>
+                    <DatePicker
+                      className="dateform"
+                      selected={startDate}
+                      name="from"
+                      onChange={(date) => {
+                        setStartDate(date);
+                        setFilter({ ...filter, from: date.toISOString() });
+                      }}
+                      selectsStart
+                      startDate={startDate}
+                      endDate={endDate}
+                      dateFormat="dd/MM/yyyy"
+
+                    />
+                  </div>
+                  <div>
+                    <label className="datelabel-to" htmlFor="endDate">
+                      {" "}
+                      To &nbsp; {"      "}
+                    </label>
+                    <DatePicker
+                      className="dateform"
+                      selected={endDate}
+                      name="to"
+                      onChange={(date) => {
+                        setEndDate(date);
+                        setFilter({ ...filter, to: date.toISOString() });
+                      }}
+                      selectsEnd
+                      endDate={endDate}
+                      minDate={startDate}
+                      dateFormat="dd/MM/yyyy"
+                      ref= {ref}
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="modalfilter">
+                <strong class="filterlabel">Tax Deductable Status</strong>
+
+                <div class="custom-control custom-radio custom-control-inline">
+                  <input
+                    type="radio"
+                    class="custom-control-input"
+                    id="defaultInline1"
+                    name="taxDeduc"
+                    onChange={() =>
+                      "taxDeduc" in filter ? delete filter.taxDeduc : null
+                    }
+                  />
+                  <label class="custom-control-label" htmlFor="defaultInline1">
+                    Any
+                  </label>
+                </div>
+
+                <div class="custom-control custom-radio custom-control-inline">
+                  <input
+                    type="radio"
+                    class="custom-control-input"
+                    id="defaultInline2"
+                    name="taxDeduc"
+                    onChange={(e) =>
+                      setFilter({ ...filter, [e.target.name]: e.target.value })
+                    }
+                    value={true}
+                  />
+                  <label class="custom-control-label" htmlFor="defaultInline2">
+                    Tax Deductible
+                  </label>
+                </div>
+
+                <div class="custom-control custom-radio custom-control-inline">
+                  <input
+                    type="radio"
+                    class="custom-control-input"
+                    id="defaultInline3"
+                    name="taxDeduc"
+                    onChange={(e) =>
+                      setFilter({ ...filter, [e.target.name]: e.target.value })
+                    }
+                    value={false}
+                  />
+                  <label class="custom-control-label" htmlFor="defaultInline3">
+                    Non Tax Deductible
+                  </label>
+                </div>
+              </div>
+
+              <div className="modalfilter">
+                <b class="filterlabel">
+                  Source contains any of these phrase(s)
+                </b>
+                <span className="donorfilter-subhead">
+                  Type in each source and press "Enter"
+                </span>
+                  <Typeahead
+                    id="sources-list"
+                    labelKey="description"
+                    multiple={true}
+                    options={sources}
+                    name="source"
+                    onChange={(selected) => {
+                      setSelectedTypeAhead(selected);
+                      setFilter({
+                        ...filter,
+                        source: selected.map((e) => {
+                          return e.description;
+                        }),
+                      });
+                    }}
+                    placeholder="e.g. Charity Dinner, Reach website"
+                    selected={selectTypeAhead}
+                    className="sources"
+                    ref= {ref}
+                  />
+              </div>
+
+              <div className="modalfilter">
+                <b class="filterlabel d-flex">Total Donated Amount</b>
+                <div
+                  class="form-inline my-2 my-lg-0"
+                  id="donationAmtSearchForm "
+                >
+                  <input
+                    className="form-control mr-sm-1 form-amt "
+                    name="minAmt"
+                    type="number"
+                    step="0.01"
+                    placeholder="$.0.00"
+                    aria-label="Search"
+                    onChange={(e) => {
+                      setFilter({ ...filter, [e.target.name]: e.target.value });
+                      /* if (filter.minAmt === "") delete filter.minAmt */
+                    }}
+                  />
+                  to&nbsp; {"      "}
+                  <input
+                    className="form-control mr-sm-2 form-amt"
+                    name="maxAmt"
+                    type="number"
+                    step="0.01"
+                    placeholder="$0.00"
+                    aria-label="Search"
+                    onChange={(e) =>
+                      setFilter({ ...filter, [e.target.name]: e.target.value })
+                    }
+                  />
+                </div>
+              </div>
             </div>
-          </div>
-        </Modal.Body>
-      </div>
-      <div className="advanced-filters">
-        <button
-          onClick={() => {}}
-          className="button transparentbutton advanced-filters-toggle"
-        >
-          <img src={Chevronright} className="button-icon" alt="right arrow" />{" "}
-          View Advanced Filters
-        </button>
-      </div>
-      <Modal.Footer>
-        <Header.Buttons>
-          <div style={{ display: "flex" }}>
-            <button
-              style={{ marginLeft: "auto" }}
-              // onClick={() => {}}
-              className="button transparentbutton"
-            >
-              Reset Filters
-            </button>
-            <button
-              onClick={()=>buildQuery()}
-              className={"button orangebutton "}
-            >
-              Apply Filters
-            </button>
-          </div>
-        </Header.Buttons>
-      </Modal.Footer>
+          </Modal.Body>
+        </div>
+        <div className="advanced-filters">
+          <button
+            onClick={() => {}}
+            className="button transparentbutton advanced-filters-toggle"
+          >
+            <img src={Chevronright} className="button-icon" alt="right arrow" />{" "}
+            View Advanced Filters
+          </button>
+        </div>
+        <Modal.Footer>
+          <Header.Buttons>
+            <div style={{ display: "flex" }}>
+              <button
+                style={{ marginLeft: "auto" }}
+                onClick={() => ref.current.clear()}
+                className="button transparentbutton"
+                type = "reset"
+              >
+                Reset Filters
+              </button>
+              <button
+                onClick={buildAndSubmitQuery}
+                className={"button orangebutton "}
+                type="button"
+              >
+                Apply Filters
+              </button>
+            </div>
+          </Header.Buttons>
+        </Modal.Footer>
+      </form>
     </Modal>
   );
 }
