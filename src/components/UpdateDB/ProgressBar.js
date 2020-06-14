c2575002828c6e033c39a328d01aa82594ee120d
Lu Khei <kheiboardwarrior@gmail.com>
2020-06-14 16:44:01 +0800

    replace progress bar and percentage with spinner

--- src/components/UpdateDB/ProgressBar.js
+++ src/components/UpdateDB/ProgressBar.js
@@ -2,40 +2,20 @@ import React, {useState, useEffect, useRef} from "react";
 import "./index.css";
 import { Line } from 'rc-progress';
 import Modal from '../Modal'
-
-
-function useInterval(callback, delay) {
-  const savedCallback = useRef()
-
-  useEffect(() => {
-    savedCallback.current = callback
-  }, [callback])
-
-  useEffect(() => {
-    function tick() {
-      savedCallback.current()
-    }
-    if (delay !== null) {
-      let id = setInterval(tick, delay)
-      return () => clearInterval(id)
-    }
-  }, [delay])
-}
+import Spin from "../../lib/spinner";
 
 
 const ProgressBar = (props) =>{
   
-  const [percent, setPercent] = useState(0)
-  useInterval(() => setPercent(p => p + 10), percent < 80 ? 100 : null)
- 
   // , if upload failed use props.onFailedUpload
     return (
       <Modal show ={true} className= "popup-box">
         <div className= "popup-loadcontainer">
+          <div className ="progress-loading">
         <h1 className= "popup-msg2">Uploading in progress</h1>
-        <p className= "popup-msg1"> Please do not close this tab</p>
-        <Line percent={percent} strokeWidth="2" strokeColor="#FFA001" trailColor="#F8E7CF" trailWidth="2"/>
-        <p className= "percent">{percent}% </p>
+        <Spin className = "progress-spinner"/>
+        </div>
+        <span className= "popup-msg1"> Please do not close this tab</span>
         </div>
       </Modal>
     )
--- src/lib/spinner.js
+++ src/lib/spinner.js
@@ -1,9 +1,9 @@
 import Spinner from 'react-bootstrap/Spinner'
 import React from 'react'
 
-const Spin = () => {
+const Spin = ({className}) => {
   return (
-    <div className="spinner">
+    <div className= {className ? className : "spinner"}>
       <Spinner animation="border" role="status">
         <span className="sr-only">Loading...</span>
       </Spinner>
